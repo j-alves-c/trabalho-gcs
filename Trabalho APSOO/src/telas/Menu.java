48bf6adb40c4e6e7d65a0c0d06960664e8671806
guilhermectprado <guilherme.prado@ufms.br>
2022-06-17 16:05:31 -0400

    feat: Adição da Tela de Cadastro Cliente

--- Trabalho APSOO/src/telas/Menu.java
+++ Trabalho APSOO/src/telas/Menu.java
@@ -6,6 +6,8 @@ import java.awt.*;
 public class Menu {
 	public JMenuItem novaVenda;
 	public JMenuItem novaDevolucao;
+	public JMenuItem novaTroca;
+	public JMenuItem novoCliente;
 	public JFrame frame;
 
 	/**
@@ -37,21 +39,16 @@ public class Menu {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 930, 582);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{101, 0};
-		gridBagLayout.rowHeights = new int[]{22, 34, 22, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		frame.getContentPane().setLayout(gridBagLayout);
-		
+
 		JMenuBar barraDeMenu = new JMenuBar();
 		frame.setJMenuBar(barraDeMenu);
-		//menu para a venda
+
+		// menu para a venda
 		JMenu menuVenda = new JMenu("VENDA");
 		barraDeMenu.add(menuVenda);
 		menuVenda.setHorizontalAlignment(SwingConstants.CENTER);
 		menuVenda.setFont(new Font("Segoe UI", Font.PLAIN, 18));
-		
+
 		novaVenda = new JMenuItem("Nova Venda");
 		menuVenda.add(novaVenda);
 		novaVenda.addActionListener(e -> EventQueue.invokeLater(() -> {
@@ -59,19 +56,18 @@ public class Menu {
 				EfetuarVenda efetuarVenda = new EfetuarVenda();
 				efetuarVenda.getFrame().setVisible(true);
 
-
 			} catch (Exception f) {
 				f.printStackTrace();
 			}
 
 		}));
 
-		//menu para a devolução
+		// menu para a devoluï¿½ï¿½o
 		JMenu menuDevolucao = new JMenu("DEVOLU\u00c7\u00c3O");
 		menuDevolucao.setHorizontalAlignment(SwingConstants.CENTER);
 		menuDevolucao.setFont(new Font("Segoe UI", Font.PLAIN, 18));
 		barraDeMenu.add(menuDevolucao);
-		
+
 		novaDevolucao = new JMenuItem("Nova Devolu\u00e7\u00e3o");
 		menuDevolucao.add(novaDevolucao);
 		novaDevolucao.addActionListener(f -> EventQueue.invokeLater(() -> {
@@ -83,23 +79,41 @@ public class Menu {
 			}
 
 		}));
-		//menu para a troca
+
+		// menu para a troca
 		JMenu menuTroca = new JMenu("TROCA");
 		menuTroca.setFont(new Font("Segoe UI", Font.PLAIN, 18));
 		menuTroca.setHorizontalAlignment(SwingConstants.CENTER);
 		barraDeMenu.add(menuTroca);
-		
-		JMenuItem novaTroca = new JMenuItem("Nova Troca");
+
+		novaTroca = new JMenuItem("Nova Troca");
 		menuTroca.add(novaTroca);
 		novaTroca.addActionListener(e -> EventQueue.invokeLater(() -> {
 			try {
-				JOptionPane.showMessageDialog(null,"Funcionalidade ainda n\u00e3o implementada!");
-
+				JOptionPane.showMessageDialog(null, "Funcionalidade ainda n\u00e3o implementada!");
 
 			} catch (Exception f) {
 				f.printStackTrace();
 			}
 		}));
-		
+
+		// menu para o cliente
+		JMenu menuCliente = new JMenu("CLIENTE");
+		menuCliente.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		menuCliente.setHorizontalAlignment(SwingConstants.CENTER);
+		barraDeMenu.add(menuCliente);
+
+		novoCliente = new JMenuItem("Inserir Cliente");
+		menuCliente.add(novoCliente);
+		novoCliente.addActionListener(e -> EventQueue.invokeLater(() -> {
+			try {
+				InserirCliente inserirCliente = new InserirCliente();
+				inserirCliente.getFrame().setVisible(true);
+			} catch (Exception g) {
+				g.printStackTrace();
+			}
+
+		}));
+
 	}
 }
